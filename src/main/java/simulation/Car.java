6cc36cd0c2557173ec66e7362df9c54dbe36dcde
Joonatan Korpela <joonatan.aatos@gmail.com>
2021-10-23 13:37:42 +0300

    Create tick function for cars

--- src/main/java/simulation/Car.java
+++ src/main/java/simulation/Car.java
@@ -1,22 +1,30 @@
 package simulation;
 
 public class Car {
-    private final Route route;
+    private Route route;
     private final CarType carType;
 
     private int batteryLife;
     private double drivenDistance;
+    private double drivingSpeed;
+    private boolean hasReachedDestination;
 
     public Car(CarType carType_) {
         carType = carType_;
-        route = Route.generateRandomRoute();
 
         batteryLife = carType.capacity;
         drivenDistance = 0;
+        drivingSpeed = 120;
+        hasReachedDestination = false;
     }
 
     public void tick() {
+        double deltaDistance = drivingSpeed / 60d;
+        drivenDistance += deltaDistance;
+        batteryLife -= deltaDistance * carType.drivingEfficiency;
 
+        if (drivenDistance >= route.getLength())
+            hasReachedDestination = true;
     }
 
     public double getDrivenDistance() {
@@ -29,6 +37,14 @@ public class Car {
 
     public Route getRoute() {
         return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+
+    public boolean isHasReachedDestination() {
+        return hasReachedDestination;
     }
 
     public int getBatteryLife() {
--- src/main/java/simulation/Simulation.java
+++ src/main/java/simulation/Simulation.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Simulation {
 
     ArrayList<Car> cars;
+    int timeElapsed;
 
     public Simulation() {
         Routes.generateRoutes();
@@ -14,10 +15,33 @@ public class Simulation {
     private void createCars() {
         cars = new ArrayList<>();
         cars.add(new Car(CarType.TESLAMOTORS_MODEL3));
+
+        for (Car car : cars) {
+            car.setRoute(Route.generateRandomRoute());
+        }
     }
 
     public void start() {
         System.out.println("Starting...");
+
+        timeElapsed = 0;
+
+        while (!allCarsHaveReachedTheirDestination()) {
+            for (Car car : cars) {
+                car.tick();
+            }
+            timeElapsed++;
+            if (timeElapsed == Integer.MAX_VALUE)
+                throw new RuntimeException("Time exceeded maximum integer value");
+        }
+    }
+
+    private boolean allCarsHaveReachedTheirDestination() {
+        for (Car car : cars) {
+            if (!car.isHasReachedDestination())
+                return false;
+        }
+        return true;
     }
 
     @Override

c48f7992f591ab3bf898e237c3a2711254e0bc60
Justus Hansen <justus.hansen@paivola.fi>
2021-11-19 11:10:14 +0000

    Save statistics as CSV

--- src/main/java/simulation/Statistics.java
+++ src/main/java/simulation/Statistics.java
@@ -66,9 +66,42 @@ public class Statistics {
             return;
         }
 
-        printWriter.println(this.toString());
+        printWriter.println(this.toCSV());
 
         printWriter.close();
+    }
+
+    public String toCSV() {
+        StringBuilder s = new StringBuilder();
+        s.append("Total cars: ;").append(totalCars).append("\n");
+        s.append("Total time (s): ;").append(totalTime).append("\n\n");
+
+        // State statistics
+        long totalStateTime = LongStream.of(stateStatistics).sum() - stateStatistics[4];
+        Car.State[] states = Car.State.values();
+        s.append("Average state statistics:\n");
+
+        s.append("State;Time (min/car);Percentage\n");
+        for (int i = 0; i < states.length; i++) {
+            if (i == 4)
+                continue;
+            s.append(states[i].toString()).append(";").append((double) stateStatistics[i] / (double) (totalCars*60L) + ";").append((double) stateStatistics[i] / (double) totalStateTime * 100d + "").append("\n");
+        }
+        s.append("\n\n");
+        s.append(String.format("Total time driving; %.0f; min/car\n", (double) totalStateTime / (double) (totalCars*60L)));
+
+
+        // Traffic statistics
+        int totalTraffic = Arrays.stream(trafficStatistics).sum();
+        s.append("\nTraffic statics:\n");
+        s.append("HeLa;").append(trafficStatistics[0]).append(String.format(";%.1f", (double) trafficStatistics[0] / (double) totalTraffic * 100d)).append("\n");
+        s.append("LaJy;").append(trafficStatistics[1]).append(String.format(";%.1f", (double) trafficStatistics[1] / (double) totalTraffic * 100d)).append("\n");
+        s.append("JyOu;").append(trafficStatistics[2]).append(String.format(";%.1f", (double) trafficStatistics[2] / (double) totalTraffic * 100d)).append("\n");
+        s.append("OuKe;").append(trafficStatistics[3]).append(String.format(";%.1f", (double) trafficStatistics[3] / (double) totalTraffic * 100d)).append("\n");
+        s.append("KeRo;").append(trafficStatistics[4]).append(String.format(";%.1f", (double) trafficStatistics[4] / (double) totalTraffic * 100d)).append("\n");
+        s.append("RoUt;").append(trafficStatistics[5]).append(String.format(";%.1f", (double) trafficStatistics[5] / (double) totalTraffic * 100d)).append("\n");
+        s.append("\n");
+        return s.toString();
     }
 
     @Override
